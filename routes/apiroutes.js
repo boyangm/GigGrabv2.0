b365f9c4d98c426750dbdd47999b268651179a4e
boyangmatsapola <boyangbass@gmail.com>
2019-10-24 23:03:38 -0500

    redeploy

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,11 +1,8 @@
 const router = require("express").Router();
 const axios = require('axios');
 
-const MY_API_KEY = process.env.MY_API_KEY || 'AFeiQyudCRNK8T2g46sKFz';
-const client = require('filestack-js').init(MY_API_KEY);
  
-// router.get('api/', (req,res) =>{
-
-// })
+router.route('/users')
+    .post()
 
 module.exports = router;
