b9e500b6a251622d11376fae78772ed60c96fea2
boyangmatsapola <boyangbass@gmail.com>
2019-10-24 21:44:58 -0500

    save for the day

--- giggrab/src/profile.js
+++ giggrab/src/profile.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-
+import { API } from './utils/api'
+import * as filestack from 'filestack-js';
+const client = filestack.init('****');
 
 class Profile extends Component {
     state = {
@@ -12,7 +14,6 @@ class Profile extends Component {
 
     }
 
-
     redirectToTarget = () => {
         this.props.history.push('/');
     }
@@ -20,17 +21,17 @@ class Profile extends Component {
     handleChange = (e) => {
         const { name, value } = e.target;
         const instruments = this.state.instruments;
-        name === 'instruments' && this.state.instruments.indexOf(value)=== -1
-        ? this.setState({[name]: [...instruments, value] })
-        :
-        this.setState({ [name]: value })
+        name === 'instruments' && this.state.instruments.indexOf(value) === -1
+            ? this.setState({ [name]: [...instruments, value] })
+            :
+            this.setState({ [name]: value })
         return console.log(this.state);
     }
 
 
     handleSubmit = (e) => {
         e.preventDefault();
-        const { name, password, instruments, image,  email, bio } = this.state;
+        const { name, password, instruments, image, email, bio } = this.state;
         this.setState({
             name,
             email,
@@ -54,28 +55,43 @@ class Profile extends Component {
         const instrument = e.target.value;
         const prevState = this.state.instruments;
 
-        if (prevState.length > 0){
-            prevState.map( item =>{
-    
-                if ( item === instrument){
-                    const removed = prevState.filter( item =>{
-                        return item != instrument;
+        if (prevState.length > 0) {
+            prevState.map(item => {
+
+                if (item === instrument) {
+                    const removed = prevState.filter(item => {
+                        return item !== instrument;
                     })
-                    this.setState({instruments: removed})
-                }else{
-    
-                    this.setState({instruments: [...prevState,instrument]})
+                    return this.setState({ instruments: removed })
+                } else {
+
+                    return this.setState({ instruments: [...prevState, instrument] })
                 }
             })
-            
-        }else{
-            this.setState({instruments: [...prevState,instrument]})
-            
+
+        } else {
+            this.setState({ instruments: [...prevState, instrument] })
+
         }
-      };
+    };
+
+    getImage = (event) => {
+        const files = event.target.files;
+        const file = files.item(0);
+
+        client.upload(file)
+            .then(res => {
+                console.log('success: ', res)
+                this.setState({ image: res.url });
+                console.log(this.state);
+            })
+            .catch(err => {
+                console.log(err)
+            });
+    }
 
     renderData = data => {
-        fetch('/api/contact', {
+        fetch('/api/users', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -99,25 +115,25 @@ class Profile extends Component {
                 <h3>Create your Profile!</h3>
                 <form onSubmit={this.handleSubmit} ref={(el) => this.myFormRef = el}>
                     <label for="name">Name:</label>
-                    <input onChange={this.handleChange} value={this.state.name} type='text'  name='name' />
+                    <input onChange={this.handleChange} value={this.state.name} type='text' name='name' />
                     <label for="password">Password:</label>
                     <input onChange={this.handleChange} value={this.state.password} type='text' name='password' />
                     <label for="email">Email:</label>
                     <input onChange={this.handleChange} value={this.state.email} type='text' name='email' />
                     <label for="proImage">Profile Image:</label>
-                    <input id="fileInput"  type="file" accept="image/png, image/jpeg" value ={this.state.image}/>
+                    <input id="fileInput" type="file" accept="image/png, image/jpeg" onChange={this.getImage} />
                     <label for="bio">Bio:</label>
                     <textarea onChange={this.handleChange} value={this.state.bio} name='bio' />
                     <label for="start">Instrument:</label>
-                    <select className= "instrumentbox" multiple = {true} value={this.state.instruments} onChange={this.handleInstrumentChange}>
-                    <option value= "Guitar">Guitar</option>
-                    <option value="Bass">Bass</option>
-                    <option value="Drums">Drums</option>
-                    <option value="Vocals">Vocals</option>
-                    <option value="Keys">Keys</option>
-                    <option value="Other">Other</option>
-                </select>
-                <h3>{this.state.instruments.map(item => `${item}, `)}</h3>
+                    <select className="instrumentbox" multiple={true} value={this.state.instruments} onChange={this.handleInstrumentChange}>
+                        <option value="Guitar">Guitar</option>
+                        <option value="Bass">Bass</option>
+                        <option value="Drums">Drums</option>
+                        <option value="Vocals">Vocals</option>
+                        <option value="Keys">Keys</option>
+                        <option value="Other">Other</option>
+                    </select>
+                    <h3>{this.state.instruments.map(item => `${item}, `)}</h3>
                     <button type="submit">Submit</button>
                 </form>
             </div>
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 // const sgMail = require('@sendgrid/mail');
 // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
+const routes = require("./routes");
 const app = express();
 
 app.use(bodyParser.json());
@@ -14,8 +14,8 @@ app.use(express.json());
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/giggrab";
 mongoose.connect(MONGODB_URI)
-    .then(() => console.log('Mongo Db connected'))
-    .catch(err => console.log(err));
+.then(() => console.log('Mongo Db connected'))
+.catch(err => console.log(err));
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('abantudev/build'))
@@ -24,6 +24,7 @@ if (process.env.NODE_ENV === 'production') {
     })
 }
 
+app.use(routes);
 
 var PORT = process.env.PORT || 5000;
 
