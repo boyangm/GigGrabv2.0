4631809299d25789822295f53e1e0e053cd982e0
boyangmatsapola <boyangbass@gmail.com>
2019-11-27 07:10:18 -0600

    auth revert back

--- giggrab/src/components/activity.js
+++ giggrab/src/components/activity.js
@@ -1,39 +1,29 @@
-import React, { useContext, useEffect } from 'react'
-import { NavLink, Route, Redirect } from 'react-router-dom'
-import { Consumer } from './gigcontext'
+import React ,{ useContext , useEffect} from 'react'
+import {NavLink, Route, Redirect} from 'react-router-dom'
+import {Consumer} from './gigcontext'
 import GigsEmployed from './gigsemployed'
 import GigsEmploying from './gigsemploying'
-import { GigsContext } from './gigcontext'
+import{GigsContext} from './gigcontext'
 
 
 // updates the  gigs hosted and empoloyed of the user
-const Activity = (props) => {
+const Activity = (props) =>{
   const context = useContext(GigsContext)
-  const { state, actions, isAuth } = context
-  useEffect(() => {
+  const {state, actions} = context
+  useEffect(() =>{
     actions.fetchOneUser(state.localUser._id)
-  }, [])
-  const { match } = props;
-  return (
-    <div className="activityCont">
-      {
-        isAuth
-          ? (
-            <>
-              <ul>
-                <li><NavLink to={`${match.url}/employed`}>Gigs employed</NavLink> </li>
-                <li><NavLink to={`${match.url}/employing`}>Gigs employing</NavLink> </li>
-              </ul>
-              <Route exact path={match.path} render={() => <Redirect to={`${match.path}/employed`} />} />
-              <Route path={`${match.path}/employed`} render={(props) => <GigsEmployed {...props} />} />
-              <Route path={`${match.path}/employing`} render={(props) => <GigsEmploying {...props} />} />
-            </>
-          )
-          : (
-            props.history.push('/login')
-          )
-      }
-    </div>
-  )
+  } , [])
+    const {match} = props;
+    return(
+      <div className="activityCont">
+        <ul>
+            <li><NavLink to = {`${match.url}/employed`}>Gigs employed</NavLink> </li>
+            <li><NavLink to = {`${match.url}/employing`}>Gigs employing</NavLink> </li>
+        </ul>
+        <Route exact path={match.path} render = {() => <Redirect to ={`${match.path}/employed`}/>}/>
+        <Route path= {`${match.path}/employed`} render= {(props) =><GigsEmployed {...props}/>}/>
+        <Route path= {`${match.path}/employing`} render= {(props) =><GigsEmploying {...props}/>}/>
+      </div>
+    )
 }
 export default Activity
