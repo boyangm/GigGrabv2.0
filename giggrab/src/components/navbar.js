b6e058d447e878c01ea8f85052e2ec305e68fdd4
boyangmatsapola <boyangbass@gmail.com>
2019-12-29 02:02:46 -0600

    fixed ux of navbar

--- giggrab/src/components/navbar.js
+++ giggrab/src/components/navbar.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState}from 'react';
 import { NavLink } from 'react-router-dom'
-import { Consumer } from './gigcontext'
+import { Consumer, GigsContext } from './gigcontext'
 
 // routes you to different parts of the app
 const NavBar = () => {
+    const [navState, setNavState] = useState('navbar') 
+    const handleScroll =() =>{
+        if(window.scrollY > 119){
+            setNavState('navbar scrolled')
+        }else{
+          setNavState('navbar')
+        }
+      }
+
+    useEffect(() =>{
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+
+    },[])
+
+
     return (
         <Consumer>
             {context =>
                 context.state.isAuth
 
                 ?(<div className="navbar">
-                    <h3 className='title'> Giggrab</h3>
+                <NavLink exact to="/home" className="title" >Giggrab</NavLink>
                     <div className="links" >
-                        <NavLink exact to="/home" className="linkItems" >Home</NavLink>
+                        
                         <NavLink exact to='/activity' className="linkItems" >Activity</NavLink>
                         <NavLink exact to='/talentpool' className="linkItems">Talent Pool</NavLink>
                         <NavLink onClick={context.actions.logout} exact to="/logout" className="linkItems" >Logout</NavLink>
